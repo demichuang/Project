0bc40353f8fc89e3f1997e0468c05f2c3d503d5e
hi <here>
2016-07-23 02:46:02 +0000

    Merge branch 'two',conflict solved

--- achievement.php
+++ achievement.php
@@ -1,9 +1,11 @@
 <?php
-header('Content-type: text/html; charset=utf-8');
-include_once("mysql.php");
-$Table="gone";
+header('Content-type: text/html; charset=utf-8');   //使用萬用字元碼utf-8
+include_once("mysql.php");                          // 連結資料庫new
+$Table="dst";       // 取dat資料表(影響：no按鈕)
+$Table1="file";     // 取file資料表(影響：no按鈕)
+$Table2="file2";    // 取file2資料表(影響：no按鈕)
 
-session_start();
+session_start();    // 啟動session(使用：$_SESSION['userName'])
 ?>
 
 <!DOCTYPE html>
@@ -21,9 +23,7 @@ session_start();
 <link rel="stylesheet" href="assets/gallery/blueimp-gallery.min.css">
 <link rel="shortcut icon" href="images/favicon.ico" type="image/x-icon">
 <link rel="icon" href="images/favicon.ico" type="image/x-icon">
-
 <link rel="stylesheet" href="assets/style.css">
-
 </head>
 
 
@@ -35,9 +35,10 @@ session_start();
   <div class="container">
     <div class="navbar navbar-inverse navbar-fixed-top" role="navigation" id="top-nav">
       <div class="container">
-        
+          
+        <!-- 顯示使用者名稱 -->
         <a class="navbar-brand active"><h2><?php echo $_SESSION['userName'];?></h2></a>
-            <!-- Nav Starts -->
+           
             <div class="navbar-collapse  collapse">
               <ul class="nav navbar-nav navbar-right">
                 
@@ -50,44 +51,55 @@ session_start();
                
               </ul>
             </div>
-            <!-- #Nav Ends -->
+            
       </div>
      </div>
    </div>
 </div>
+<h1>1</h1>
 <!-- Header Ends -->
 
-<h1>1</h1>
+
 <?php
 
-$result=mysqli_query($conn,"SELECT * FROM $Table 
-                            WHERE username ='{$_SESSION['userName']}'
-                            AND d=1"); 
-$result2=mysqli_query($conn,"SELECT * FROM $Table 
-                            WHERE username ='{$_SESSION['userName']}'
-                            AND d=2");                            
-$gone = mysqli_num_rows($result);
-$gone2 = mysqli_num_rows($result2);
-$gonenumber = round(($gone/3)*100,2);
-$gonenumber2 = round(($gone2/3)*100,2);
+$row1=mysqli_num_rows(mysqli_query($conn,"SELECT * FROM $Table WHERE d=1"));    // 從dst資料夾取Taichung的景點數
+$row2=mysqli_num_rows(mysqli_query($conn,"SELECT * FROM $Table WHERE d=2"));    // 從dst資料夾取Tainan的景點數
 
-// Taichung Number  
+// 從file資料夾取去過的Taichung景點數
+$result1=mysqli_query($conn,"SELECT * FROM $Table1
+                            WHERE username ='{$_SESSION['userName']}'
+                            AND gone=1"); 
+$gone = mysqli_num_rows($result1);
+
+// 從file2資料夾取去過的Tainan景點數
+$result2=mysqli_query($conn,"SELECT * FROM $Table2
+                            WHERE username ='{$_SESSION['userName']}'
+                            AND gone=1");                       
+$gone2 = mysqli_num_rows($result2);
+
+$gonenumber = round(($gone/$row1)*100,2);       // 計算去過Taichung景點數的%
+$gonenumber2 = round(($gone2/$row2)*100,2);     // 計算去過Tainan景點數的%
+
+
 echo "<div class='overlay spacer'>
         <div class='container'>
           <div class='row text-center'>";
-          
-if($gone>0){               
-    while($row =mysqli_fetch_array($result))
+
+// 取Taichung去過的景點          
+if($gone>0){
+    // 取每筆資料
+    while($row =mysqli_fetch_array($result1))
     {
-        echo "<h4>{$row['dname']}
-            <a href='travel_done.php?gone={$row['dname']}'>no gone</a>
-            </h4>";       
+        echo "<h4>{$row['dname']}";                                     // 印出景點名
+        echo "<a href='travel_done.php?gone={$row['dname']}'>no</a>";   // 刪除景點
+        echo "</h4>";       
     }
     echo"</div>
         </div>
        </div>";
 }
 
+// 印出Taichung的%
 echo "<div class='highlight-info'>
         <div class='container'>
           <div class='row text-center  wowload fadeInDownBig'> 
@@ -96,22 +108,27 @@ echo "<div class='highlight-info'>
         </div>
      </div>";
 
- // Tainan Number
+
+
+ 
 echo "<div class='overlay spacer'>
         <div class='container'>
           <div class='row text-center'>";
-if($gone2>0){               
+// 取Tainan去過的景點 
+if($gone2>0){ 
+    // 取每筆資料
     while($row =mysqli_fetch_array($result2))
     {
-        echo "<h4>{$row['dname']}
-            <a href='travel_done.php?gone={$row['dname']}'>no gone</a>
-            </h4>";       
+        echo "<h4>{$row['dname']}";                                     // 印出景點名
+        echo "<a href='travel_done.php?gone={$row['dname']}'>no</a>";   // 刪除景點
+        echo "</h4>";       
     }
     echo"</div>
         </div>
        </div>";
 }
 
+// 印出Tainan的%
 echo "<div class='highlight-info'>
         <div class='container'>
           <div class='row text-center  wowload fadeInDownBig'>
